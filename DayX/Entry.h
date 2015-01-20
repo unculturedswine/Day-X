56324b6e8e87aff697a13ae1143c80c7d1714021
Joel Jenkins <jj@joeljenkins.me>
2015-01-20 15:32:30 -0700

    Step 10 Complete

--- DayX/Entry.h
+++ DayX/Entry.h
@@ -8,10 +8,17 @@
 
 #import <UIKit/UIKit.h>
 
+static NSString * const titleKey = @"title";
+static NSString * const textKey = @"text";
+static NSString * const timestampKey = @"timestamp";
+
 @interface Entry : NSObject
 
 @property (nonatomic, strong) NSString *title;
 @property (nonatomic, strong) NSString *text;
 @property (nonatomic, strong) NSDate *timestamp;
+
+-(NSDictionary *)entryDictionary;
+-(id)initWithDictionary:(NSDictionary *)dictionary;
 
 @end
--- DayX/Entry.m
+++ DayX/Entry.m
@@ -10,4 +10,27 @@
 
 @implementation Entry
 
+-(NSDictionary *)entryDictionary {
+    NSMutableDictionary *entryDictionary = [NSMutableDictionary new];
+    if (self.title) {
+        [entryDictionary setObject:self.title forKey:titleKey];
+    }
+    if (self.text) {
+        [entryDictionary setObject:self.text forKey:textKey];
+    }
+    if (self.timestamp) {
+        [entryDictionary setObject:self.timestamp forKey:timestampKey];
+    }
+    return entryDictionary;
+}
+-(id)initWithDictionary:(NSDictionary *)dictionary {
+    self = [super init];
+    if (self) {
+        self.title = dictionary[titleKey];
+        self.text = dictionary[textKey];
+        self.timestamp = dictionary[timestampKey];
+    }
+    return self;
+}
+
 @end
